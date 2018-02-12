4d3db1ff50d854c5ceed0295d0ed1427808aa06e
HolliNiesen <holli.niesen@gmail.com>
2018-02-12 13:51:11 -0600

    Changing double quotes to single quotes

--- src/test/resources/resetDb.sql
+++ src/test/resources/resetDb.sql
@@ -1,2 +1,2 @@
 TRUNCATE user;
-INSERT INTO user VALUES (1, "Holli", "1993-02-16", 1, 1), (2, "Cosmo", "2012-05-18", 2, 1);
+INSERT INTO user VALUES (1, 'Holli', '1993-02-16', 1, 1), (2, 'Cosmo', '2012-05-18', 2, 1);
